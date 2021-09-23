37157d9f977920ef8a74a6048aa6343e45d53412
julian3493 <julianramosarevalo@gmail.com>
2021-09-23 18:19:05 -0500

    add routing to project and modify productlinstin component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
-import { BrowserRouter, Switch} from 'reac'
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
 import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
+import ProductDetail from './containers/ProductDetail';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing}/>
+          <Route path="/product/:productId" exact component={ProductDetail}/>
+          <Route>404 Not found!</Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const ProductComponent = () => {
+  const products = useSelector((state) => state.allProducts.products);
+  const { id, title } = products[0];
   return (
-     <div>
-       <h1>ProductComponent</h1>
+     <div className="four column wide">
+       <div className="ui link cards">
+         <div className="card">
+           <div className="image">
+           </div>
+           <div className="content">
+             <div className="header">
+               {title}
+             </div>
+           </div>
+         </div>
+       </div>
      </div>
   )
 }
